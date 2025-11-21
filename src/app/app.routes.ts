779d5b569f8f26ff584395ba0ddd6a22803d7fd9
Ahmet S. <Salih2660akkaymak@gmail.com>
2025-11-21 09:27:27 +0100

    yes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { Login } from './login/login';
+<<<<<<< Updated upstream
 
+=======
+import { CinemaSelector } from './cinema-selector/cinema-selector';
+import { TopNavbar } from './components/top-navbar/top-navbar';
+>>>>>>> Stashed changes
 
 export const routes: Routes = [
 
+<<<<<<< Updated upstream
     {
         path: 'home',
         component: Home,
@@ -26,4 +32,29 @@ export const routes: Routes = [
         pathMatch: 'full'
     }
 
+=======
+  // pages WITHOUT top nav
+  {
+    path: 'cinema-selector',
+    component: CinemaSelector,
+    title: 'Select Cinema'
+  },
+
+  // pages WITH top nav
+  {
+    path: '',
+    component: TopNavbar,   // contains <nav> + <router-outlet>
+    children: [
+      { path: 'home', component: Home, title: 'Home Page' },
+      { path: 'login', component: Login, title: 'Login Page' }
+    ]
+  },
+
+  // wildcard
+  {
+    path: '**',
+    redirectTo: 'cinema-selector',
+    pathMatch: 'full'
+  }
+>>>>>>> Stashed changes
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,18 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+<<<<<<< Updated upstream
 import { TopNavbar } from "./components/top-navbar/top-navbar";
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, TopNavbar],
+=======
+
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+>>>>>>> Stashed changes
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
