cd1eb0396f1cf5f36d8dfc5ad3b3e3e3aa3271cf
Ahmet S. <Salih2660akkaymak@gmail.com>
2025-11-21 09:43:38 +0100

    fix hopefully

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,44 +1,10 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { Login } from './login/login';
-<<<<<<< Updated upstream
-
-=======
-import { CinemaSelector } from './cinema-selector/cinema-selector';
 import { TopNavbar } from './components/top-navbar/top-navbar';
->>>>>>> Stashed changes
 
 export const routes: Routes = [
 
-<<<<<<< Updated upstream
-    {
-        path: 'home',
-        component: Home,
-        title: 'Home Page'
-    },
-    {
-        path: 'login',
-        component: Login,
-        title: 'Login Page'
-    },
-    {
-        path: '',
-        redirectTo: '/home',
-        pathMatch: 'full'
-    },
-    {
-        path: '**',
-        redirectTo: '/home',
-        pathMatch: 'full'
-    }
-
-=======
-  // pages WITHOUT top nav
-  {
-    path: 'cinema-selector',
-    component: CinemaSelector,
-    title: 'Select Cinema'
-  },
 
   // pages WITH top nav
   {
@@ -56,5 +22,4 @@ export const routes: Routes = [
     redirectTo: 'cinema-selector',
     pathMatch: 'full'
   }
->>>>>>> Stashed changes
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,18 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-<<<<<<< Updated upstream
-import { TopNavbar } from "./components/top-navbar/top-navbar";
-
-@Component({
-  selector: 'app-root',
-  imports: [RouterOutlet, TopNavbar],
-=======
 
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
->>>>>>> Stashed changes
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
