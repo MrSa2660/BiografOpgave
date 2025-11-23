6a434c61a158f83e73c438e5ab73c039093251e7
Ahmet S. <Salih2660akkaymak@gmail.com>
2025-11-23 21:07:59 +0100

    home page

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+// src/app/home/home.component.ts
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule, FormsModule],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './home.html',
-  styleUrl: './home.css',
+  styleUrls: ['./home.css'],
 })
-export class Home {}
+export class Home implements OnInit {
+  city: string | null = null;
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((params) => {
+      this.city = params.get('city');
+    });
+  }
+}
