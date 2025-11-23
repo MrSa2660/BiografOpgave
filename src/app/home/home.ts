8c4b8f6601181d9f36bfd59bd77754bf9d3ea6e0
Ahmet S. <Salih2660akkaymak@gmail.com>
2025-11-23 14:46:03 +0100

    changes

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,19 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, RouterLink } from '@angular/router';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
-
-
 
 @Component({
   selector: 'app-home',
-  imports: [ CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home  {
-  
-}
-
-  
+export class Home {}
