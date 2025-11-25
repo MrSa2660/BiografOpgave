9d1a083b9a3091728732a159cce164247c23e41f
Ahmet S. <Salih2660akkaymak@gmail.com>
2025-11-25 13:11:59 +0100

    service and dummy data

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -2,6 +2,10 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { MovieService } from '../services/movie.service';
+import { Movie } from '../models/movie.model';
+
+
 
 @Component({
   selector: 'app-home',
@@ -12,12 +16,28 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class Home implements OnInit {
   city: string | null = null;
+  highlightMovie: Movie | null = null;
+  movies: Movie[] = [];
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private movieService: MovieService
+  ) {}
 
   ngOnInit(): void {
     this.route.paramMap.subscribe((params) => {
       this.city = params.get('city');
+
+      // get data from the service, based on the city
+      this.highlightMovie = this.movieService.getHighlightForCity(this.city);
+      this.movies = this.movieService.getMoviesByCity(this.city);
+
+      // Optionally, don't show the highlight movie again in the list
+      if (this.highlightMovie) {
+        this.movies = this.movies.filter(
+          (m) => m.id !== this.highlightMovie!.id
+        );
+      }
     });
   }
 }
